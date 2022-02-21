071ea752a5904c98669c0beb3ccddab339ce0cbf
zahqresh <muhammadhamza296@gamil.com>
2022-02-21 10:44:35 +0500

    images update

--- utils/discord/add_role.js
+++ utils/discord/add_role.js
@@ -7,60 +7,69 @@ module.exports = (client) => {
       //specific channel channel to verify
       console.log(msg.content);
       if (msg.content.includes("!verify")) {
-        db.findOne({ discord_id: msg.author.id }).then((doc) => {
-          //return member.roles.add(member.guild.roles.cache.get('919279049946308628')); //Issuing a role
-          if (doc != null) {
-            msg.channel.send("You have a new role now...");
-            let johnMember = msg.guild.members.cache.get(`${doc.discord_id}`);
-            let mcRole; //this would be the specific role id to give to people
-            switch (`${doc.registered_for}`) {
-              case "tester":
-                mcRole = msg.guild.roles.cache.get(`${process.env.tester}`);
-                break;
-              case "whitelist_role":
-                mcRole = msg.guild.roles.cache.get(
-                  `${process.env.whitelist_role}`
-                );
-                break;
-              case "beast_master":
-                mcRole = msg.guild.roles.cache.get(
-                  `${process.env.beast_master}`
-                );
-                break;
-              case "warrior_master":
-                mcRole = msg.guild.roles.cache.get(
-                  `${process.env.warrior_master}`
-                );
-                break;
-              case "legion_master":
-                mcRole = msg.guild.roles.cache.get(
-                  `${process.env.legion_master}`
-                );
-                break;
-              case "monster_conqueror":
-                mcRole = msg.guild.roles.cache.get(
-                  `${process.env.warrior_conqueror}`
-                );
-                break;
-              case "king_of_nicah":
-                mcRole = msg.guild.roles.cache.get(
-                  `${process.env.king_of_nicah}`
-                );
-                break;
+        db.findOne({ discord_id: msg.author.id })
+          .then((doc) => {
+            //return member.roles.add(member.guild.roles.cache.get('919279049946308628')); //Issuing a role
+            if (doc != null) {
+              msg.channel.send("`NEW ROLE ASSIGNED TO YOUR PROFILE!`");
+              let johnMember = msg.guild.members.cache.get(`${doc.discord_id}`);
+              let mcRole; //this would be the specific role id to give to people
+              switch (`${doc.registered_for}`) {
+                case "tester":
+                  mcRole = msg.guild.roles.cache.get(`${process.env.tester}`);
+                  break;
+                case "whitelist_role":
+                  mcRole = msg.guild.roles.cache.get(
+                    `${process.env.whitelist_role}`
+                  );
+                  break;
+                case "beast_master":
+                  mcRole = msg.guild.roles.cache.get(
+                    `${process.env.beast_master}`
+                  );
+                  break;
+                case "warrior_master":
+                  mcRole = msg.guild.roles.cache.get(
+                    `${process.env.warrior_master}`
+                  );
+                  break;
+                case "legion_master":
+                  mcRole = msg.guild.roles.cache.get(
+                    `${process.env.legion_master}`
+                  );
+                  break;
+                case "monster_conqueror":
+                  mcRole = msg.guild.roles.cache.get(
+                    `${process.env.warrior_conqueror}`
+                  );
+                  break;
+                case "king_of_nicah":
+                  mcRole = msg.guild.roles.cache.get(
+                    `${process.env.king_of_nicah}`
+                  );
+                  break;
 
-              default:
-                break;
+                default:
+                  break;
+              }
+              if (!mcRole) return;
+              if (!johnMember) return;
+              if (johnMember.roles.cache.has(mcRole.id)) return;
+              johnMember.roles.add(mcRole.id);
             }
-            if (!mcRole) return;
-            if (!johnMember) return;
-            if (johnMember.roles.cache.has(mcRole.id)) return;
-            johnMember.roles.add(mcRole.id);
-          }
-          if (doc == null) {
-            console.log("You are not registered and cant be verified...");
-            msg.channel.send("You are not registered and cant be verified...");
-          }
-        });
+            if (doc == null) {
+              console.log("Unverified user running command!");
+              msg.channel.send(
+                "`USER NOT FOUND IN REGISTERATION!`"
+              );
+            }
+          })
+          .then(() => {
+            db.findOneAndDelete({ discord_id: msg.author.id }).then((data) => {
+             // console.log(data);
+              console.log(`${data} deleted after adding role!`);
+            });
+          });
       }
     }
   });
