a8d430f52b64fcf803c62596e4ba0170f4e15c41
Muhammad Hamza Qureshi <muhammadhamza2965@gmail.com>
2022-03-13 22:09:06 +0500

    ui update

--- utils/discord/add_role.js
+++ utils/discord/add_role.js
@@ -20,12 +20,7 @@ module.exports = (client) => {
           .then((doc) => {
             //return member.roles.add(member.guild.roles.cache.get('919279049946308628')); //Issuing a role
             if (doc != null) {
-              msg.channel
-                .reply("Invalid command!")
-                .then((msg) => {
-                  setTimeout(() => msg.delete(), 10000);
-                })
-                .catch(console.error);
+              msg.channel.send("`NEW ROLE ASSIGNED TO YOUR PROFILE!`");
               let johnMember = msg.guild.members.cache.get(`${doc.discord_id}`);
               let mcRole; //this would be the specific role id to give to people
               switch (`${doc.registered_for}`) {
@@ -73,12 +68,7 @@ module.exports = (client) => {
             }
             if (doc == null) {
               console.log("Unverified user running command!");
-              msg.channel
-                .reply("Invalid command!")
-                .then((msg) => {
-                  setTimeout(() => msg.delete(), 10000);
-                })
-                .catch(console.error);
+              msg.channel.send("`USER NOT FOUND IN REGISTERATION!`");
             }
           })
           .then(() => {
