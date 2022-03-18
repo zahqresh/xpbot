a9227d598d88f942ce28ae77c25eb66993a8b598
Muhammad Hamza Qureshi <muhammadhamza2965@gmail.com>
2022-03-18 10:28:38 +0500

    command added

--- utils/discord/discord.js
+++ utils/discord/discord.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const { Client, Intents, MessageEmbed } = require("discord.js");
 const add_role = require("./add_role");
+const role = require("./role");
 //const create_invite = require("./create_invite");
 
 // Create a new client instance
@@ -22,7 +23,7 @@ client.once("ready", () => {
 //create_invite(client);
 
 add_role(client);
-
+role(client)
 
 client.login(process.env.BOT_TOKEN); //the bot token
 console.log(process.env.BOT_TOKEN);
