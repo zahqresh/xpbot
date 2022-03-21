ecf753e34352391d3b64993c93c4f964c8f68774
Muhammad Hamza Qureshi <muhammadhamza2965@gmail.com>
2022-03-21 14:19:32 +0500

    admin id update

--- app.js
+++ app.js
@@ -16,27 +16,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  // set locals, only providing error in development
-  res.locals.msg = req.flash("msg");
-  res.locals.type = req.flash("type");
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
 
-  // render the error page
-  res.status(err.status || 500);
-  console.log(err);
-  res.render("error");
-});
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   console.log(err);
+//   res.render("error");
+// });
 //get the bot up
 require("./utils/discord/discord");
 
@@ -44,8 +42,12 @@ app.listen(process.env.PORT || 5000, () => {
   console.log("API ONLINE!");
 });
 
+app.get("/api", (req, res) => {
+  res.send("Online...");
+});
+
 setInterval(() => {
-  axios.get("https://ludufi-bot.herokuapp.com/").then(() => {
+  axios.get("https://ludufi-bot.herokuapp.com/api").then(() => {
     console.log("Awaking ludufi bot... 👀");
   });
 }, 20000);
